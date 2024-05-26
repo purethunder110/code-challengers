2eeb57c426b4725e037b316424199f98e024a4dd
Aniket Yadav <83213012+purethunder110@users.noreply.github.com>
2024-05-26 20:40:21 +0530

    create initial setup with environment setup

---
+++ frontend/src/Element.jsx
@@ -0,0 +1,13 @@
+import {Container,Typography} from "@mui/material";
+
+
+function App(){
+    return (
+    <Container>
+        <Typography>Hello World</Typography>
+    </Container>
+    );
+    
+}
+
+export default App
